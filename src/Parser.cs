1818e5b9742a91ac8b7c4b168ad9d7e5ce29881b
Erik Zapata <zapatabrandon@gmail.com>
2016-06-23 16:33:06 -1200

    First Commit
    First commit, DOM is mostly complete but I need to add some more
    functionality to it. Right now it does not print out an tag attributes
    and Parser is still not fully complete.

---
+++ src/Parser.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// Parses HTML into a DOM object
+/// </summary>
+
+namespace ToyBrowser.src
+{
+    public class Parser
+    {
+        Dom.Node head;
+        uint pos;
+        string input;
+
+        public Parser()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        // returns character at position
+        private char nextChar()
+        {
+            if (endOfFile())
+            {
+                throw new Exception("Position is greater than length of input string");
+            }
+            return input[(int)pos];
+        }
+
+        // checks if input string starts with string s
+        private bool startWith(string s)
+        {
+            return input.StartsWith(s);
+        }
+
+        // checks to see if we've reached the end of the input string
+        private bool endOfFile()
+        {
+            return pos >= input.Length;
+        }
+
+        // returns characater at position and increments position
+        private char consumeChar()
+        {
+            char next = nextChar();
+            pos++;
+            return next;
+        }
+
+        /// <summary>
+        /// increments pos until the current char at pos is not whitespace
+        /// </summary>
+        private void consumeWhitespace()
+        {
+            // C# has shortcircuiting yayyy
+            while (!endOfFile() && String.IsNullOrWhiteSpace(nextChar().ToString()))
+            {
+                pos++;
+            }
+        }
+
+        /// <summary>
+        /// parses a tag or attribute name. like html or id="thing"
+        /// </summary>
+        /// <returns> returns attibute name or tag name</returns>
+        private string parseTagName()
+        {
+            StringBuilder strBuilder = new StringBuilder();
+            while (!endOfFile() && (Char.IsLetterOrDigit(nextChar())))
+            {
+                strBuilder.Append(nextChar());
+            }
+            return strBuilder.ToString();
+        }
+
+        private Dom.Node parseNode()
+        {
+            Dom.Node thing = new Dom.Node();
+            if (nextChar() == '<')
+            {
+
+            }
+
+            return thing;
+        }
+
+        private Dom.Node parseElement()
+        {
+            return new Dom.Node();
+        }
+
+        private Dom.Node parseText()
+        {
+            return new Dom.Node();
+        }
+    }
+}
---
+++ src/dom.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// Dom data struct for html. I'm trying to write my own simple web browser
+/// we'll see how that goes
+/// </summary>
+
+namespace ToyBrowser.src
+{
+    class Dom
+    {
+        public enum NodeType
+        {
+            Text, Element
+        };
+
+        public class Node
+        {
+            protected NodeType nodeType;
+
+            protected Node[] children;                        // will be null whenever node type is Text 
+            protected string text;                            // if nodeType is text then this will be non-null
+            protected string tag;                             // non empty when nodeType is Element
+            protected Dictionary<string, string> attributes;  // also non null whenever nodeType is Element
+
+            public Node()
+            {
+                throw new Exception("Don't use this constructor");
+            }
+
+            public Node(NodeType t, string text, string tag, Dictionary<string, string> attr, Node[] children)
+            {
+                this.nodeType = t;
+                if (t == NodeType.Text)
+                {
+                    this.text = text;
+                    this.tag = null;
+                    this.attributes = null;
+                    this.children = null;
+                }
+                else
+                {
+                    this.text = null;
+                    this.tag = tag;
+                    this.attributes = attr;
+                    this.children = children;
+                }
+            }
+
+            public override string ToString()
+            {
+                StringBuilder strBuilder = new StringBuilder();
+                int tabber = 0;
+
+                this.addToStringBuilder(strBuilder, tabber);
+
+                return strBuilder.ToString();
+            }
+
+            public void addToStringBuilder(StringBuilder strBuilder, int tabber)
+            {
+                // could also try using switch but there's only two nodetype right so whatever
+                if (nodeType == NodeType.Element)
+                {
+                    StringBuilder closer = new StringBuilder();
+                    for (int i = 0; i < tabber; i++)
+                    {
+                        strBuilder.Append("\t");
+                    }
+
+                    strBuilder.Append("<");
+                    strBuilder.Append(tag);
+                    // add in attributes here later
+                    strBuilder.Append(">");
+
+                    closer.Append("\n");
+                    // adding this later
+                    for (int i = 0; i < tabber; i++)
+                    {
+                        closer.Append("\t");
+                    }
+                    closer.Append("<\\");
+                    closer.Append(tag);
+                    closer.Append(">");
+
+                    foreach (Node c in children)
+                    {
+                        strBuilder.Append("\n");
+                        c.addToStringBuilder(strBuilder, tabber + 1);
+                    }
+
+                    strBuilder.Append(closer.ToString());
+                }
+                else if (nodeType == NodeType.Text)         // redundant but makes it easier for reading i guess
+                {
+                    for (int i = 0; i < tabber; i++)
+                    {
+                        strBuilder.Append("\t");
+                    }
+                    strBuilder.Append(text);
+                }
+                else
+                {
+                    // do nothing!
+                }
+            }
+        }
+
+        // static since I'll be manipulating Node and not class Dom
+        public static Node createText(string text)
+        {
+            return new Node(NodeType.Text, text, null, null, null);
+        }
+
+        public static Node createElement(string tag, Dictionary<string, string> attr, Node[] children)
+        {
+            return new Node(NodeType.Element, null, tag, attr, children);
+        }
+    }
+}
